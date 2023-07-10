713adf1167d7f8afaa7a8a8cb937d711e2dfad28
Pedro Arthur <pedro13arthur@gmail.com>
2023-07-10 20:43:21 -0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { SendGridModule } from '@anchan828/nest-sendgrid';
+import { CodesModule } from './codes/codes.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { SendGridModule } from '@anchan828/nest-sendgrid';
       apikey: process.env.SEND_GRID_API_KEY,
     }),
     UsersModule,
+    CodesModule,
   ],
 })
 export class AppModule {}
