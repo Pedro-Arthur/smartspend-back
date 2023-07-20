86f943190c91a9fa33336704064c8f1f414a40c9
Pedro Arthur <pedro13arthur@gmail.com>
2023-07-20 12:58:32 -0300

    bank codes

--- src/database/seeds/BankSeeder.ts
+++ src/database/seeds/BankSeeder.ts
@@ -1,15 +1,11 @@
 import { Seeder } from 'typeorm-extension';
 import { DataSource } from 'typeorm';
 import { Bank } from 'src/banks/banks.entity';
+import bankCodes from 'src/utils/bankCodes';
 
 export default class BankSeeder implements Seeder {
   public async run(dataSource: DataSource): Promise<any> {
     const repository = dataSource.getRepository(Bank);
-    await repository.insert([
-      {
-        code: 'aaa',
-        name: 'bbbbbbbb',
-      },
-    ]);
+    await repository.insert(bankCodes);
   }
 }
